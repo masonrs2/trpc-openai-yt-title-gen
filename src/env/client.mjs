13d475576443fe00fc20a1fa55e0becc22d50035
Mason <masonsepulveda@gmail.com>
2023-02-02 23:08:03 -0600

    fix type return error

--- src/env/client.mjs
+++ src/env/client.mjs
@@ -15,10 +15,7 @@ export const formatErrors = (
     .filter(Boolean);
 
 if (!_clientEnv.success) {
-  console.error(
-    "❌ Invalid environment variables:\n",
-    ...formatErrors(_clientEnv.error.format()),
-  );
+
   throw new Error("Invalid environment variables");
 }
 
