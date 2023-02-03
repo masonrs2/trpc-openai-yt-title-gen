fe7275b54f9946bc00a0b62d22693d69e27d40e9
Mason <masonsepulveda@gmail.com>
2023-02-02 23:09:40 -0600

    fix type return error

--- src/env/server.mjs
+++ src/env/server.mjs
@@ -9,10 +9,7 @@ import { env as clientEnv, formatErrors } from "./client.mjs";
 const _serverEnv = serverSchema.safeParse(serverEnv);
 
 if (!_serverEnv.success) {
-  console.error(
-    "❌ Invalid environment variables:\n",
-    ...formatErrors(_serverEnv.error.format()),
-  );
+  
   throw new Error("Invalid environment variables");
 }
 
